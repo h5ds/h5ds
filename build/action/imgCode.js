23abe2d49b8f92c309c07d6aff16438168a096cc
qq676015863 <676015863@qq.com>
2017-09-04 17:50:06 +0800

    更新 imgCode.js

--- build/action/imgCode.js
+++ build/action/imgCode.js
@@ -1,27 +1,5 @@
 var captchapng = require('captchapng');
 
-// /*
-// ** randomWord 产生任意长度随机字母数字组合
-// ** randomFlag-是否任意长度 min-任意长度最小位[固定位数] max-任意长度最大位
-// ** xuanfeng 2014-08-28
-// */
-// function randomWord(randomFlag, min, max) {
-//     var str = "",
-//         range = min,
-//         arr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-
-//     // 随机产生
-//     if (randomFlag) {
-//         range = Math.round(Math.random() * (max - min)) + min;
-//     }
-//     for (var i = 0; i < range; i++) {
-//         var pos = Math.round(Math.random() * (arr.length - 1));
-//         console.log(pos);
-//         str += arr[pos];
-//     }
-//     return str;
-// }
-
 // 获取验证码
 exports.imgCode = function (req, res) {
     var code = parseInt(Math.random()*9000+1000);
