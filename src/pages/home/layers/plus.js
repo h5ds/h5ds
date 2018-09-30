630d39b9317023c7af27ad7fc75d0b553ad2ef2d
mantou <676015863@qq.com>
2018-09-30 14:55:49 +0800

    add

--- src/pages/home/layers/plus.js
+++ src/pages/home/layers/plus.js
@@ -1,7 +1,5 @@
 import { H5DSPlus } from './plusClasses.js';
 import PhoneDom from './dom/phone.js';
-import QrcodeDom from './qrcode/phone.js';
-import testDom from './test/phone.js';
 
 // 装载插件
-new H5DSPlus([PhoneDom, QrcodeDom, testDom]);
+new H5DSPlus([PhoneDom]);
