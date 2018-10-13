46756f268629ddb6676023a4c3a1e80c4477747d
mantou <676015863@qq.com>
2018-10-13 18:12:39 +0800

    修复选中组的BUG 添加testVersion模式，testVersion模式下，可以不用传data参数

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,12 +1,12 @@
 import './style.less';
-import './h5ds/css/h5ds.4.2.0.css'; // 编辑器样式
+import 'h5ds/css/h5ds.4.2.0.css'; // 编辑器样式
 import './layers/plus'; // 手机端会执行的代码
 
 import * as layers from './layers';
 
 import React, { Component } from 'react'; // ...
 
-import H5DS from './h5ds'; // ..
+import H5DS from 'h5ds'; // ..
 import { Modal } from 'antd';
 import SyntaxHighlighter from 'react-syntax-highlighter';
 import { github } from 'react-syntax-highlighter/styles/hljs';
@@ -22,64 +22,71 @@ import pretty from 'pretty';
  * @param fail function 失败时
  */
 export default class HomePage extends Component {
-  state = {
-    visible: false,
-    shtml: ''
-  };
+    state = {
+        visible: false,
+        shtml: ''
+    };
 
-  // 保存当前页面
-  savePage = (appid, cdata) => {
-    console.log('保存当前页面', appid, cdata);
-    return new Promise((resolve, reject) => {
-      // ...
-      resolve();
-    });
-  };
+    // 保存当前页面
+    savePage = (appid, cdata) => {
+        console.log('保存当前页面', appid, cdata);
+        return new Promise((resolve, reject) => {
+            // ...
+            resolve();
+        });
+    };
 
-  // 保存 app
-  saveApp = (appid, cdata) => {
-    console.log('保存APP', appid, cdata);
-    return new Promise((resolve, reject) => {
-      // ...
-      resolve();
-    });
-  };
+    // 保存 app
+    saveApp = (appid, cdata) => {
+        console.log('保存APP', appid, cdata);
+        return new Promise((resolve, reject) => {
+            // ...
+            resolve();
+        });
+    };
 
-  // 发布
-  publishApp = (appid, cdata, shtml) => {
-    // console.log('发布应用', appid, cdata, shtml);
-    return new Promise((resolve, reject) => {
-      this.setState({ visible: true, shtml: pretty(shtml) });
-      resolve();
-    });
-  };
+    // 发布
+    publishApp = (appid, cdata, shtml) => {
+        // console.log('发布应用', appid, cdata, shtml);
+        return new Promise((resolve, reject) => {
+            this.setState({ visible: true, shtml: pretty(shtml) });
+            resolve();
+        });
+    };
 
-  handleCancel = () => {
-    this.setState({ visible: false });
-  };
+    handleCancel = () => {
+        this.setState({ visible: false });
+    };
 
-  render() {
-    const { visible, shtml } = this.state;
-    return (
-      <div>
-        <H5DS
-          appid="h5ds_demoid"
-          config={{ appHost: '', appCDN: 'http://cdn.h5ds.com/lib', backUrl: '/' }}
-          uploadSet={{}}
-          savePage={this.savePage}
-          saveApp={this.saveApp}
-          publishApp={this.publishApp}
-          plugins={[...Object.values(layers)]}
-        />
-        <Modal width={1200} title="生成代码" visible={visible} onOk={this.handleCancel} onCancel={this.handleCancel}>
-          <div className="codes-detail">
-            {/* <pre>{shtml}</pre> */}
-            <SyntaxHighlighter language="htmlbars" style={github}>
-              {shtml}
-            </SyntaxHighlighter>
-          </div>
-        </Modal>
-      </div>
-    );
-  }
+    render() {
+        const { visible, shtml } = this.state;
+        return (
+            <div>
+                <H5DS
+                    testVersion={true}
+                    appid="h5ds_demoid"
+                    config={{ appHost: '', appCDN: 'http://cdn.h5ds.com/lib', backUrl: '/' }}
+                    uploadSet={{}}
+                    savePage={this.savePage}
+                    saveApp={this.saveApp}
+                    publishApp={this.publishApp}
+                    plugins={[...Object.values(layers)]}
+                />
+                <Modal
+                    width={1200}
+                    title="生成代码"
+                    visible={visible}
+                    onOk={this.handleCancel}
+                    onCancel={this.handleCancel}
+                >
+                    <div className="codes-detail">
+                        {/* <pre>{shtml}</pre> */}
+                        <SyntaxHighlighter language="htmlbars" style={github}>
+                            {shtml}
+                        </SyntaxHighlighter>
+                    </div>
+                </Modal>
+            </div>
+        );
+    }
 }
